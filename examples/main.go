33eb809683d9828baadd066b31554f67f075adfa
Skynet Core <skynet.vasyl@gmail.com>
2019-01-08 19:03:08 +0100

    alpha

--- examples/main.go
+++ examples/main.go
@@ -26,6 +26,7 @@ func (s *Sales) Table() string {
 }
 
 func main() {
+
 	c, err := client.New("http://localhost:8080/v1alpha1/graphql", nil)
 	if err != nil {
 		log.Fatalln(err)
@@ -39,58 +40,11 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	sales := make([]*Sales, 0)
-	if err = resp.MapResult(&sales); err != nil {
+	geos := []string{}
+	if err = resp.Data.Path("sales.keyword").To(&geos); err != nil {
 		log.Fatalln(err)
 	}
 
-	for _, s := range sales {
-		fmt.Println(s)
-	}
-
-	resp, err = c.Execute(request.HasuraRequest(
-		request.NewMutation(`mutation ($jobs:[job_insert_input!]!){
-					insert_job(objects:$jobs){
-						affected_rows
-						returning {
-							job_id
-						}
-					}
-				}`), request.Variables{
-			"jobs": []map[string]interface{}{
-				map[string]interface{}{
-					"op_id":      244,
-					"job_type":   "sales",
-					"job_fields": map[string]interface{}{},
-				},
-			},
-		},
-	))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	dst := []map[string]interface{}{}
-
-	if err = resp.Returning(&dst); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(resp.AffectedRows())
-
-	resp, err = c.Execute(request.HasuraRequest(
-		request.Query(`query{
-					job_aggregate(where: {job_id:{_gte:0}}){
-						aggregate {
-							count
-						}
-					}
-				}`), nil,
-	))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println(resp.Aggregate())
+	fmt.Println(geos[0])
 
 }
--- response/response.go
+++ response/response.go
@@ -1,17 +1,11 @@
 package response
 
 import (
+	"bytes"
 	"encoding/json"
-	"errors"
-	"reflect"
-	"strings"
-)
 
-var returnedOps = map[string]bool{
-	"insert": true,
-	"update": true,
-	"delete": true,
-}
+	"github.com/Jeffail/gabs"
+)
 
 // Model ...
 type Model interface {
@@ -19,74 +13,44 @@ type Model interface {
 	Schema() []string
 }
 
+// Data ...
+type Data struct {
+	container *gabs.Container
+}
+
+// Container ...
+type Container struct {
+	v *gabs.Container
+}
+
+// To ...
+func (c *Container) To(t interface{}) error {
+	return json.NewDecoder(bytes.NewBuffer(c.v.EncodeJSON())).Decode(&t)
+}
+
+// Path ...
+func (d *Data) Path(p string) *Container {
+	return &Container{v: d.container.Path(p)}
+}
+
+// Container ...
+func (d *Data) Container() *gabs.Container {
+	return d.container
+}
+
+// UnmarshalJSON ...
+func (d *Data) UnmarshalJSON(db []byte) error {
+	c, err := gabs.ParseJSON(db)
+	if err != nil {
+		return err
+	}
+	d.container = c
+	return nil
+}
+
 // Response ...
 type Response struct {
-	Data         `json:"data,omitempty"`
+	Data         *Data         `json:"data,omitempty"`
 	Errors       []interface{} `json:"errors,omitempty"`
 	Subscription chan interface{}
-}
-
-// Data ...
-type Data map[string]json.RawMessage
-
-// Returning ...
-func (d Data) Returning(dst interface{}) error {
-	for k, v := range d {
-		if ss := strings.Split(k, "_"); len(ss) > 1 && returnedOps[ss[0]] {
-			m := make(map[string]json.RawMessage, 0)
-			if err := json.Unmarshal(v, &m); err != nil {
-				return err
-			}
-			return json.Unmarshal(m["returning"], dst)
-		}
-	}
-	return errors.New("no returning objects")
-}
-
-// MapResult ...
-func (d Data) MapResult(m interface{}) error {
-
-	t := reflect.TypeOf(m)
-	if t.Kind() != reflect.Ptr && t.Elem().Kind() != reflect.Slice {
-		return errors.New("argument must have *[]response.Model type")
-	}
-
-	inst := reflect.New(t.Elem().Elem()).Elem().Interface()
-	ni, ok := inst.(Model)
-	if !ok {
-		return errors.New("argument must have *[]response.Model type")
-	}
-	if r, ok := d[ni.Table()]; ok {
-		return json.Unmarshal(r, m)
-	}
-	return nil
-}
-
-// AffectedRows ...
-func (d Data) AffectedRows() (int, error) {
-	for k, v := range d {
-		if ss := strings.Split(k, "_"); len(ss) > 1 && returnedOps[ss[0]] {
-			m := make(map[string]json.RawMessage, 0)
-			if err := json.Unmarshal(v, &m); err != nil {
-				return 0, err
-			}
-			var rows int
-			return rows, json.Unmarshal(m["affected_rows"], &rows)
-		}
-	}
-	return 0, errors.New("no returning objects")
-}
-
-// Aggregate ...
-func (d Data) Aggregate() map[string]interface{} {
-	for k, v := range d {
-		if ss := strings.Split(k, "_"); len(ss) > 1 && ss[1] == "aggregate" {
-			m := make(map[string]interface{}, 0)
-			if err := json.Unmarshal(v, &m); err != nil {
-				return nil
-			}
-			return m
-		}
-	}
-	return nil
 }
